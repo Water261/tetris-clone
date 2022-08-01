a30587ad3dbe469fedcebbcf9e6195025229e1aa
Matthew Wilks <water261@outlook.com.au>
2022-08-01 17:25:03 +1000

    feat: Add generated rectangles Colours are randomly selected from the tetris colours

--- src/index.js
+++ src/index.js
@@ -3,30 +3,56 @@
 import Phaser from "phaser";
 
 class MyGame extends Phaser.Scene {
-  preload() {
-    this.load.image("logo", "src/assets/logo.png");
-  }
+	preload() {
+		this.load.image("logo", "src/assets/logo.png");
+	}
 
-  create() {
-    const logo = this.add.image(250, 150, "logo");
+	create() {
+		this.graphics = {
+			red: this.addNewGraphics(0xff0000),
+			orange: this.addNewGraphics(0xffa500),
+			yellow: this.addNewGraphics(0xffff00),
+			purple: this.addNewGraphics(0x800080),
+			blue: this.addNewGraphics(0x0000ff),
+			cyan: this.addNewGraphics(0x00ffff),
+			green: this.addNewGraphics(0x00ff00),
+		};
+		this.colours = [
+			"red",
+			"orange",
+			"yellow",
+			"purple",
+			"blue",
+			"cyan",
+			"green",
+		];
 
-    this.tweens.add({
-      targets: logo,
-      y: 600,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1,
-    });
-  }
+		const rectangleA = new Phaser.Geom.Rectangle(0, 0, 150, 150);
+		const randNum = Math.floor(Math.random() * this.colours.length);
+		const randColour = this.colours[randNum];
+
+		this.graphics[randColour].fillRectShape(rectangleA);
+	}
+
+	/**
+	 * Adds a new graphics object and returns it
+	 * @param {number} colour 
+	 */
+	addNewGraphics(colour) {
+		return this.add.graphics({
+			fillStyle: {
+				color: colour,
+			},
+		});
+	}
 }
 
 const config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 500,
-  height: 800,
-  scene: MyGame,
+	type: Phaser.AUTO,
+	parent: "phaser-example",
+	width: 500,
+	height: 800,
+	scene: MyGame,
 };
 
 const game = new Phaser.Game(config);
